79af8d2fc18ac24659be522bcd46f4af6f76ab88
Amy Pellegrini <amyp@motabilityoperations.co.uk>
2020-03-20 15:17:34 +0000

    Render all black squares in their proper place

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,24 +1,24 @@
 const iframe = document.getElementById("js-chess-iframe");
 
 function test(message, testFn) {
-  const testDescription = "It should render a chess board";
+  const testDescription = message;
   let testResult = testDescription;
 
   try {
     const expected = testFn();
 
     if (expected) {
-      testResult += " PASSED";
+      testResult += " PASSED\n";
     }
   } catch (error) {
-    testResult += ` FAILED\n${error}`;
+    testResult += ` FAILED\n>>>> ${error}`;
   }
 
-  document.getElementById("testResults").innerText = testResult;
+  document.getElementById("testResults").innerText += testResult;
 }
 
 function runTestSuite() {
-  test("It should render a chess board", () => {
+  test("It renders a chess board", () => {
     const chessBoard = iframe.contentWindow.document.getElementById(
       "js-chess-board"
     );
@@ -27,6 +27,48 @@ function runTestSuite() {
       return true;
     } else {
       throw new Error("Chess board element not found");
+    }
+  });
+
+  test("It renders 32 black squares", () => {
+    const chessBoard = iframe.contentWindow.document.getElementById(
+      "js-chess-board"
+    );
+
+    const blackSquares = chessBoard.getElementsByTagName("rect");
+
+    if (blackSquares.length === 32) {
+      return true;
+    } else {
+      throw new Error("Not enough black squares rendered");
+    }
+  });
+
+  test("Black squares are placed properly", () => {
+    const chessBoard = iframe.contentWindow.document.getElementById(
+      "js-chess-board"
+    );
+
+    const blackSquares = [...chessBoard.getElementsByTagName("rect")];
+
+    let lastX;
+    let lastY;
+
+    blackSquares.forEach(square => {
+      const x = square.getAttribute("x");
+      const y = square.getAttribute("y");
+
+      misplaced =
+        !(x % 45 === 0 && x !== lastX) && !(y % 45 === 0 && y !== lastY);
+
+      lastX = x;
+      lastY = y;
+    });
+
+    if (!misplaced) {
+      return true;
+    } else {
+      throw new Error("Black squares not properly placed");
     }
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const boardDOMElement = document.getElementById("js-chess-board");
-const square = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
-square.setAttribute("width", 45);
-square.setAttribute("height", 45);
-square.setAttribute("x", 0);
-square.setAttribute("y", 0);
-square.setAttribute("style", "fill:rgb(0,0,0);");
+let xAxis = 0;
+let yAxis = 0;
 
-boardDOMElement.appendChild(square);
+for (let i = 0; i < 32; i++) {
+  const square = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+  square.setAttribute("width", 45);
+  square.setAttribute("height", 45);
+  square.setAttribute("x", xAxis);
+  square.setAttribute("y", yAxis);
+  square.setAttribute("style", "fill:rgb(0,0,0);");
+
+  xAxis += 90;
+
+  if (xAxis > 90 * 4 - 45) {
+    xAxis = yAxis % 2 !== 0 ? 0 : 45;
+    yAxis += 45;
+  }
+
+  boardDOMElement.appendChild(square);
+}
